b6eb2906611be1864264b91b6016fbf4f631da7f
Nathan Zhang <nml707805@gmail.com>
2024-09-16 01:39:35 +1200

    Optimized course fetching in plans when opening/closing update plan modals from plan page by delegating the modal to a child component of DegreePlan

--- src/components/degree/DegreePlan.jsx
+++ src/components/degree/DegreePlan.jsx
@@ -1,7 +1,7 @@
-import React, { useContext, useState } from 'react';
-import { useParams, useLocation, useNavigate } from "react-router-dom";
+import React, { useContext } from 'react';
+import { useParams, useNavigate } from "react-router-dom";
 import Term from './Term';
-import { UpdatePlanModal } from '../dashboard/PlanModal';
+import UpdatePlan from './UpdatePlan';
 import { UserContentContext } from '../../context/UserContentContext';
 import styles from '../../styles/degree/DegreePlan.module.css';
 
@@ -37,7 +37,6 @@ function generateTerms(startTerm, startYear, endTerm, endYear, summer, system) {
 function DegreePlan() {
     const { id } = useParams();  // Get the dynamic id from the URL
     const { plans, updatePlan, deletePlan } = useContext(UserContentContext);
-    const [isUpdateModalOpen, setUpdateModalOpen] = useState(false);
     const navigate = useNavigate();
 
     const plan = plans.find((plan) => plan.id === id);
@@ -54,33 +53,19 @@ function DegreePlan() {
 
     const handleUpdatePlan = (updatedPlan) => {
         updatePlan(updatedPlan);
-        setUpdateModalOpen(false);
     };
     
     return (
         <div className={styles.rootContainer}>
             <div className={styles.planHeaderContainer}>
                 <h2 className={styles.planHeader}>{plan.name || "Degree Plan"}</h2>
-                <div className={styles.planSettings}>
-                    <svg onClick={() => setUpdateModalOpen(true)} xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
-                        <path fill="currentColor" d="M19.14 12.94c.04-.3.06-.61.06-.94c0-.32-.02-.64-.07-.94l2.03-1.58a.49.49 0 0 0 .12-.61l-1.92-3.32a.488.488 0 0 0-.59-.22l-2.39.96c-.5-.38-1.03-.7-1.62-.94l-.36-2.54a.484.484 0 0 0-.48-.41h-3.84c-.24 0-.43.17-.47.41l-.36 2.54c-.59.24-1.13.57-1.62.94l-2.39-.96c-.22-.08-.47 0-.59.22L2.74 8.87c-.12.21-.08.47.12.61l2.03 1.58c-.05.3-.09.63-.09.94s.02.64.07.94l-2.03 1.58a.49.49 0 0 0-.12.61l1.92 3.32c.12.22.37.29.59.22l2.39-.96c.5.38 1.03.7 1.62.94l.36 2.54c.05.24.24.41.48.41h3.84c.24 0 .44-.17.47-.41l.36-2.54c.59-.24 1.13-.56 1.62-.94l2.39.96c.22.08.47 0 .59-.22l1.92-3.32c.12-.22.07-.47-.12-.61zM12 15.6c-1.98 0-3.6-1.62-3.6-3.6s1.62-3.6 3.6-3.6s3.6 1.62 3.6 3.6s-1.62 3.6-3.6 3.6" />
-                    </svg>
-                </div>
+                <UpdatePlan plan={plan} handleUpdatePlan={handleUpdatePlan} handleDeletePlan={handleDeletePlan}/>
             </div>
             <div className={styles.planContainer}>
                 {terms.map(term => (
                     <Term key={Math.random()} term={term} degreePlan={id} />
                 ))}
             </div>
-            {isUpdateModalOpen && (
-                <UpdatePlanModal
-                    plan={plan}
-                    onUpdatePlan={handleUpdatePlan}
-                    onDeletePlan={handleDeletePlan}
-                    onClose={() => setUpdateModalOpen(false)}
-                    isOpen={isUpdateModalOpen}
-                />
-            )}
         </div>
     );
 }
