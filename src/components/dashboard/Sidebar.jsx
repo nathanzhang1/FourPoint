5a0108aba97d20cc75cd93d442bb9d7e6df0f441
Nathan Zhang <nml707805@gmail.com>
2024-09-19 00:51:09 +1200

    Degree tab in sidebar navigates to default plan when clicked

--- src/components/dashboard/Sidebar.jsx
+++ src/components/dashboard/Sidebar.jsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import { UserContentContext } from '../../context/UserContentContext';
 import styles from "../../styles/dashboard/Sidebar.module.css";
 
 export default function Sidebar() {
     const navigate = useNavigate();
+    const { plans } = useContext(UserContentContext);
+
     const handleHomeClick = () => {
         navigate("/");
     };
+
+    const defaultPlan = plans.find((plan) => plan.defaultplan === "Yes") || plans[0];
+
+    const handleDegreeClick = () => {
+        const { id } = defaultPlan;
+        if (!id) {
+            navigate("/"); // Redirect home if no plans exist
+        }
+        else {
+            navigate(`/plan/${id}`, { state: { ...defaultPlan } });
+        }
+    }
 
     return (
         <div className={styles.sidebar}>
@@ -33,7 +48,7 @@ export default function Sidebar() {
                 </svg>
                 Schedule
             </div>
-            <div className={styles.tab}>
+            <div className={styles.tab} onClick={handleDegreeClick}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-mortarboard-fill" viewBox="0 0 16 16">
                     <path d="M8.211 2.047a.5.5 0 0 0-.422 0l-7.5 3.5a.5.5 0 0 0 .025.917l7.5 3a.5.5 0 0 0 .372 0L14 7.14V13a1 1 0 0 0-1 1v2h3v-2a1 1 0 0 0-1-1V6.739l.686-.275a.5.5 0 0 0 .025-.917z"/>
                     <path d="M4.176 9.032a.5.5 0 0 0-.656.327l-.5 1.7a.5.5 0 0 0 .294.605l4.5 1.8a.5.5 0 0 0 .372 0l4.5-1.8a.5.5 0 0 0 .294-.605l-.5-1.7a.5.5 0 0 0-.656-.327L8 10.466z"/>
